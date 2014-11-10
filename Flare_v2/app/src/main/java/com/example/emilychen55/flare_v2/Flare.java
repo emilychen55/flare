15ec3828c571f9762a66bfa80a8f90b89d438479
Emily Chen <emilychen55@gmail.com>
2014-11-10 01:49:54 -0800

    updating android repo

--- Flare_v2/app/src/main/java/com/example/emilychen55/flare_v2/Flare.java
+++ Flare_v2/app/src/main/java/com/example/emilychen55/flare_v2/Flare.java
@@ -3,6 +3,7 @@ package com.example.emilychen55.flare_v2;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,11 +12,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.content.Intent;
-import android.net.Uri;
 import android.widget.Button;
-import android.view.View.OnClickListener;
-
-
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import android.view.Menu;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.os.Vibrator;
+import android.util.Log;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Flare extends Activity {
 
@@ -23,15 +34,39 @@ public class Flare extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flare);
-        //addListenerOnButton();
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-
+        Parse.initialize(this, "yC0kRgQVWDOjKZfvWtJrITDbzdDmEEqg6WKrjXoc", "6CSoQcuB7XNewa7Q7h9gC25dbsQCi5HCegJspxis");
     }
 
+    private Timer timer = new Timer();
+    private TimerTask timerTask;
+    public void dataStream(View view) {
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("LightStatus");
+                query.orderByDescending("createdAt");
+                query.getFirstInBackground(new GetCallback<ParseObject>() {
+                    public void done(ParseObject object, ParseException e) {
+                        TextView lightQuant = (TextView) findViewById(R.id.lightQuantity);
+                        if (e == null) {
+                            // object will be your game score
+                            lightQuant.setText(Integer.toString(object.getInt("value")) + " ADC");
+                            Log.d("value", Integer.toString(object.getInt("value")));
+                        } else {
+                            // something went wrong
+                            lightQuant.setText("n/a");
+                        }
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask, 0, 1000);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -68,21 +103,30 @@ public class Flare extends Activity {
         }
     }
 
-    /**
-     * Buttons
-     */
-    public void addListenerOnButton() {
-        //Select a specific button to bundle it with the action we want
-        //Button wifiButton = (Button) findViewById(R.id.wifi_btn);
-        Button onButton = (Button) findViewById(R.id.lightOn_btn);
-        //Button offButton = (Button) findViewById(R.id.lightOff_btn);
+    // Switch
+    public void switchInit(View view) {
+        boolean on = ((Switch) view).isChecked();
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (on) {
+            // Enable Vibrate for 500 milliseconds and lightOn
+            lightOn();
+            v.vibrate(500);
+        } else {
+            // Disable Vibrate and lightOff
+            lightOff();
+            v.cancel();
+        }
+    }
 
-        onButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent openBrowser =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.javacodegeeks.com"));
-                startActivity(openBrowser);
-            }
-        });
+    // Lighting functions
+    public void lightOn() {
+        ParseObject lightCommand = new ParseObject("LightCommand");
+        lightCommand.put("command", 1);
+        lightCommand.saveInBackground();
+    }
+    public void lightOff() {
+        ParseObject lightCommand = new ParseObject("LightCommand");
+        lightCommand.put("command", 0);
+        lightCommand.saveInBackground();
     }
 }
